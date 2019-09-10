fa99368889fde0aeefd6014c457d5a63580594c7
Mac021 <joaopedroamais@gmail.com>
2019-09-09 19:49:47 -0600

    edit.: Added footer,changed section background color to white

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 let nav = document.getElementById('navbar');
 let menu = document.getElementById('menu');
 
-menu.addEventListener('mouseenter',function(){
-	console.log('Hovering Menu');
-})
+// menu.addEventListener('mouseenter',function(){
+// 	menu.style.opacity = "1";
+// });
+
+// menu.addEventListener('mouseleave',function(){
+// 	menu.style.opacity = "0";
+// });
